bcb19d9e52a3b84cdb28bb6886d88f1bea26cb1f
Oss Dev <oalatrista@gmail.com>
2024-05-06 00:15:15 -0500

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by MacBook on 6/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
